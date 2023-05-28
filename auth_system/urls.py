5971c9d7d570720e0d7d05737bc5615f8c986c8d
Denis Nganga <denisnganga16@gmail.com>
2023-05-28 21:36:50 +0300

    adding the html on the reset password functionality

--- auth_system/urls.py
+++ auth_system/urls.py
@@ -9,12 +9,18 @@ urlpatterns = [
     path('register/',Register,name="register-page"),
     path('login/',Login,name="login-page"),
 
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name= "reset_password"),
+    path('reset_password/',
+          auth_views.PasswordResetView.as_view(template_name="auth_system/password_reset.html"),
+            name= "reset_password"),
 
-    path('reset_password_sent/', auth_views.PasswordResetView.as_view(), name="passwor_reset_done"),
+    path('reset_password_sent/', 
+         auth_views.PasswordResetView.as_view(template_name="auth_system/password_reset_sent.html"), 
+         name="password_reset_done"),
 
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetView.as_view(), name= "password_reset_confirm"),
 
-    path('reset_password_complete/', auth_views.PasswordResetView.as_view(), name="password_reset_complete"),
+    path('reset_password_complete/', 
+         auth_views.PasswordResetView.as_view(template_name="auth_system/password_reset_done.html"), 
+         name="password_reset_complete"),
     
     ]
