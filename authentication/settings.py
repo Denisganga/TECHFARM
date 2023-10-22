09441e54bf4749a987b528bf6db8c8c48c0891f3
Denisganga <denisnganga16@gmail.com>
2023-10-22 06:57:35 +0300

    my commit

--- authentication/settings.py
+++ authentication/settings.py
@@ -133,21 +133,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = 'static/'
 
-import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'home_page/static'),
-    os.path.join(BASE_DIR, 'auth_system/static'),
-]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+import os
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
  
 
  
